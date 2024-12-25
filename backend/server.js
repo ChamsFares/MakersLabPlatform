f6cee4dc58cb211bb966ce704c68d969995c2f2e
FaresChams <78964444+ChamsFares@users.noreply.github.com>
2024-12-25 13:00:13 +0100

    initial start

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const robotsRoutes = require("./routes/robots");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/robots", robotsRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
