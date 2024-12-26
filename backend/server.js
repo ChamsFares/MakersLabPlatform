9a9a4007a1cfea0013a3d4199c46da8ce527118c
FaresChams <78964444+ChamsFares@users.noreply.github.com>
2024-12-26 19:40:22 +0100

    set-up the backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const robotsRoutes = require("./routes/robots");
-
+//const robotsRoutes = require("./routes/robots");
+require("dotenv").config({ path: "../.env" });
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/robots", robotsRoutes);
+app.use("/api/robots", (res, req) => {
+  console.log("");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
